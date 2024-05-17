3d2d4fa3107ec205041827a7961c33f56eccbd56
vmaffioli <vinimaffioli@gmail.com>
2024-05-17 04:19:05 -0300

    feat: desempate rules

--- chatbot/chatbot_processor.js
+++ chatbot/chatbot_processor.js
@@ -1,27 +1,33 @@
 export async function getResponse(input) {
-    const inputList = input.split(" ")
+    const inputList = inputTreatment(input);
     const wordsJSON = await fetchJSON('./chatbot/data/chatbot_words.json');
     const wordsDATA = JSON.parse(JSON.stringify(wordsJSON));
     const inputWordsId = [];
 
     for (let i = 0; i < inputList.length; i++) {
-
-
         Object.values(wordsDATA).forEach(obj => {
             const { id, words } = obj;
             Object.values(words).forEach(word => {
+               // word = " " + word + " "
 
-                if (inputList[i].includes(word)) {
+                if (inputList[i] === word) {
                     inputWordsId.push(id)
                 }
             });
         });
 
-        if (inputWordsId[i] === undefined) {
+        console.log(inputWordsId[i])
+
+        if (inputWordsId[i] !== inputList[i]) {
             inputWordsId.push(inputList[i])
         }
-    }
 
+
+
+    }
+    
+    //console.log(inputList)
+    //console.log(inputWordsId)
     const responsesJSON = await fetchJSON('./chatbot/data/chatbot_responses.json');
     const responsesDATA = JSON.parse(JSON.stringify(responsesJSON));
     let responses = new Map();
@@ -31,25 +37,70 @@ export async function getResponse(input) {
 
 
         Object.values(questions).forEach(question => {
-            const questionWords = question.split(" ")
-            const questionLength = question.split(" ").length;
+            const questionWords = question.toLowerCase().split(" ");
+            const questionLength = questionWords.length;
             let responseCount = 0;
 
-            for (let i = 0; i < inputList.length; i++) {
-                if (questionWords[i] === inputWordsId[i]) {
+            //for (let i = 0; i < inputList.length; i++) {
+
+            inputWordsId.forEach(inputWord => {
+                //const inputWordToCompare = " " + inputWord + " "
+
+                questionWords.forEach(questionWord => {
+
+                if (questionWord === inputWord) {
                     ++responseCount;
                 }
-                if (responseCount === questionLength) {
+            })
+               console.log(questionWords)
+               console.log(inputWordsId)
+                console.log(questionWords.includes(inputWord))
+              console.log(questionLength + "   " + responseCount)
+                if (responseCount >= questionLength - questionLength * 0.20) {
                     responses.set(id, answer);
                 }
-            }
+            });
         });
     });
 
     if (responses.size === 1) {
         response = responses.entries().next().value[1];
     } else if (responses.size > 1) {
-        response = "estou em duvida, tenho varias respostas pra essa pergunta"
+        // response = "estou em duvida, tenho varias respostas pra essa pergunta"
+        // console.log(responses)
+
+        let resMap = new Map();
+
+        responses.forEach(res => {
+            let resCount = 0;
+
+            inputWordsId.forEach(inputWord => {
+                if (res.includes(inputWord)) {
+                    resMap.set(res, resCount)
+                    ++resCount;
+                }
+            });
+            
+        });
+
+        let maiorValor = 0;
+
+        // Itera sobre os elementos do mapa
+        resMap.forEach(function ( key, value) {
+            console.log(value)
+            console.log(key)
+
+            // Verifica se o valor atual é maior que o maior valor encontrado até agora
+            if (value > maiorValor) {
+                maiorValor = value;
+                response = key;
+               
+
+            }
+        });
+
+        console.log(resMap)
+        console.log(maiorValor + "     " + response)
     } else {
         response = "não tenho resposta pra isso"
 
@@ -70,4 +121,11 @@ async function fetchJSON(url) {
         console.error('Erro:', error);
         return null;
     }
+}
+
+function inputTreatment(input) {
+    input = input.replace(/[^\w\s]/g, '');
+    input = input.toLowerCase();
+    input = input.split(" ");
+    return input;
 }
